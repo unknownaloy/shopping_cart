685bd5ad5276ce276cd3ada52de38cdcae982207
unknownaloy <niceju4u@gmail.com>
2023-05-20 00:44:18 +0100

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+	return (
+		<div className="m-[16px]">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/store" element={<Store />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,7 @@
+const Store = () => {
+  return (
+    <div>This is the store page</div>
+  )
+}
+
+export default Store
