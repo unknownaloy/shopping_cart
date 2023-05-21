0265020fff33fb17f85c282d84cfeb33a4b59d69
unknownaloy <niceju4u@gmail.com>
2023-05-21 13:29:44 +0100

    Added StoreItem component for rendering JSON data

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,21 @@
+type StoreItemProps = {
+	id: number;
+	name: string;
+	price: number;
+	imgUrl: string;
+};
+
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+	return (
+		<div className="flex flex-col">
+			<img src={imgUrl} className="h-[200px] object-cover mb-[16px]" />
+
+			<div className="flex justify-between items-center">
+				<p>{name}</p>
+				<p>{price}</p>
+			</div>
+		</div>
+	);
+};
+
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,18 @@
-const Store = () => {
-  return (
-    <div>This is the store page</div>
-  )
-}
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
-export default Store
+const Store = () => {
+	return (
+		<div className="flex flex-col">
+			<h1>Store</h1>
+
+			<div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-[16px]">
+				{storeItems.map((item) => (
+					<div key={item.id}>{<StoreItem {...item} />}</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Store;
