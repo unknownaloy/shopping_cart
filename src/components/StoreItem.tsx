c0002df1d97bbffe2d33f548c41415393fd6206f
unknownaloy <niceju4u@gmail.com>
2023-05-21 14:39:12 +0100

    Added the 'Add to Cart' and 'Remove' buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -8,13 +8,39 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+	const quantity = 1;
+
 	return (
 		<div className="flex flex-col">
 			<img src={imgUrl} className="h-[200px] object-cover mb-[16px]" />
 
-			<div className="flex justify-between items-center">
+			<div className="flex justify-between items-center mb-[24px]">
 				<p>{name}</p>
 				<p>{formatCurrency(price)}</p>
+			</div>
+
+			<div className="flex justify-center items-center">
+				{quantity === 0 ? (
+					<button className="bg-blue-600 p-[8px] rounded-sm w-full">
+						+ Add to Cart
+					</button>
+				) : (
+					<div className="flex flex-col">
+						<div className="flex justify-center items-center space-x-[8px] mb-[24px]">
+							<button className="bg-blue-600 py-[8px] px-[16px] rounded-[8px]">
+								-
+							</button>
+							<span>{quantity} in cart</span>
+							<button className="bg-blue-600 py-[8px] px-[16px] rounded-[8px]">
+								+
+							</button>
+						</div>
+
+						<button className="bg-red-600 rounded-[8px] py-[4px] px-[8px]">
+							Remove
+						</button>
+					</div>
+				)}
 			</div>
 		</div>
 	);
